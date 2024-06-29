f3d6c204f1d6cf7c036428d2bcd7f3b25f3971be
abner <jutzuyabner@gmail.com>
2024-06-28 21:26:38 -0600

    link a archivo js

--- sis_votacion.php
+++ sis_votacion.php
@@ -8,12 +8,14 @@
 </head>
 <body>
     <div class="conReacciones">
-        <img src="./images/cara-triste.png" class="reacciones">
-        <img src="./images/indiferente.png" class="reacciones">
-        <img src="./images/cara-neutra.png" class="reacciones">
-        <img src="./images/sonreir.png" class="reacciones">
-        <img src="./images/muy-feliz.png" class="reacciones">
+        <img src="./images/cara-triste.png" class="reacciones" id="triste">
+        <img src="./images/indiferente.png" class="reacciones" id="indiferente">
+        <img src="./images/cara-neutra.png" class="reacciones" id="neutra">
+        <img src="./images/sonreir.png" class="reacciones" id="sonreir">
+        <img src="./images/muy-feliz.png" class="reacciones" id="mFeliz">
  
     </div>
+
+    <script src="js/reacciones.js"></script>
 </body>
 </html>
