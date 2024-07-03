e056d461fb2487e0a0c0f99cbd61c384d21c5fb8
abner <jutzuyabner@gmail.com>
2024-07-03 02:23:13 -0600

    ventanas

--- login.php
+++ login.php
@@ -27,8 +27,9 @@
                 </form>
             </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <!-- <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,256L60,240C120,224,240,192,360,197.3C480,203,600,245,720,229.3C840,213,960,139,1080,90.7C1200,43,1320,21,1380,10.7L1440,0L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg> -->
-</svg>
+
+    <script src="js/mostrarAlertas.js"></script>
 </body>
 </html>
