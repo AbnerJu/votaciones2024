5bd148650fa20a62f799e495a68736261bb682f2
abner <jutzuyabner@gmail.com>
2024-08-07 21:19:05 -0600

    recargar

--- mostrar_carreras.php
+++ mostrar_carreras.php
@@ -6,6 +6,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="10">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
